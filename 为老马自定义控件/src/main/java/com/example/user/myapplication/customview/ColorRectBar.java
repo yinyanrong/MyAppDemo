dca9b29095e6fc9c9a792b8223e45923e74eedfe
yyr <450742455@qq.com>
2015-11-10 19:17:52 +0800

    nnn

--- 为老马自定义控件/src/main/java/com/example/user/myapplication/customview/ColorRectBar.java
+++ 为老马自定义控件/src/main/java/com/example/user/myapplication/customview/ColorRectBar.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
@@ -45,6 +46,11 @@ public class ColorRectBar extends View {
      */
     private int rectHeight;
 
+    /**
+     *彩条对象
+     */
+    private Rect   colorRect;
+
     public ColorRectBar(Context context) {
        this(context,null);
     }
@@ -56,7 +62,9 @@ public class ColorRectBar extends View {
     public ColorRectBar(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         colorPaint =new Paint();
-        rectHeight=dip2px(context,40);
+        rectHeight=dip2px(context,20);
+        colorRect=new Rect();
+
     }
 
     @Override
@@ -64,25 +72,26 @@ public class ColorRectBar extends View {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
        int  widthMode= MeasureSpec.getMode(widthMeasureSpec);
        int  width=MeasureSpec.getSize(widthMeasureSpec);
-//       int  heightMode= MeasureSpec.getMode(widthMeasureSpec);
-//       int  height=MeasureSpec.getSize(widthMeasureSpec);
+       int  heightMode= MeasureSpec.getMode(widthMeasureSpec);
+       int  height=MeasureSpec.getSize(widthMeasureSpec);
         if(widthMode!=MeasureSpec.EXACTLY){
             width=getPaddingLeft()+getPaddingRight();
         }
-//        if(heightMode!=MeasureSpec.EXACTLY){
-//            height=getPaddingTop()+getPaddingBottom();
-//        }
+        if(heightMode!=MeasureSpec.EXACTLY){
+            height=getPaddingTop()+getPaddingBottom();
+        }
 
-        setMeasuredDimension(width,rectHeight);
+        setMeasuredDimension(width,height);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Log.i(TAG,"onDraw");
+        Log.i(TAG, "onDraw");
+        colorRect.set(0, 0, getMeasuredWidth(), rectHeight);
         colorPaint.setColor(Color.LTGRAY);
         //绘制底部浅灰色底色
-        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), colorPaint);
+        canvas.drawRect(colorRect, colorPaint);
         int   sumCircleRadius=circleRadius;
         //控件的宽度   -    2*半径  /  (八个点分7份)
         int  ss=(getWidth()-(circleRadius*2))/(showNumber-1);
@@ -90,20 +99,24 @@ public class ColorRectBar extends View {
         if(greenLine!=0){
             colorPaint.setColor(Color.GREEN);
             //绘制绿色的条
-            canvas.drawRect(0,0, greenEndwidth, getMeasuredHeight(), colorPaint);
+            canvas.drawRect(0,0, greenEndwidth, colorRect.height(), colorPaint);
         }
         if(redLine!=0){
             colorPaint.setColor(Color.RED);
             //绘制红色的条
-            canvas.drawRect(greenEndwidth, 0, greenEndwidth+ss*redLine, getMeasuredHeight(), colorPaint);
+            canvas.drawRect(greenEndwidth, 0, greenEndwidth+ss*redLine, colorRect.height(), colorPaint);
 
         }
         colorPaint.setColor(Color.GRAY);
         for (int i = 0; i <= showNumber; i++) {
             //绘制灰色的小圆点
-            canvas.drawCircle(sumCircleRadius, getHeight() / 2, circleRadius, colorPaint);
+            canvas.drawCircle(sumCircleRadius, colorRect.height() / 2, circleRadius, colorPaint);
+            String   dian=i+"点";
+            canvas.drawText(dian,0,dian.length(),sumCircleRadius,colorRect.height()*2,colorPaint);
+
             sumCircleRadius=sumCircleRadius+ss;
         }
+
     }
     /**
      * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
